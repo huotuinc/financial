4535f043af6a0995c8667b65310af2acd775840a
jin346952524 <346952524@qq.com>
2016-08-23 17:22:22 +0800

    添加config

---
+++ src/main/java/com/huotu/financial/config/CoreConfig.java
@@ -0,0 +1,19 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
+
+package com.huotu.financial.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by jin on 2016/8/23.
+ */
+@Configuration
+public class CoreConfig {
+}
