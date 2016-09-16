25a37664e79f4e6f6a4d2379a1151c65170294c9
jin346952524 <346952524@qq.com>
2016-09-16 17:39:05 +0800

    cache

--- src/main/java/com/huotu/financial/entity/FinancialBuyFlow.java
+++ src/main/java/com/huotu/financial/entity/FinancialBuyFlow.java
@@ -1,3 +1,12 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
+
 package com.huotu.financial.entity;
 
 import com.huotu.financial.enums.FinancialStatus;
@@ -17,6 +26,7 @@ import java.util.Date;
 @Setter
 @Entity
 @Table(name = "Mall_FinancialBuyFlow")
+@Cacheable(value = false)
 public class FinancialBuyFlow {
 
     /**
